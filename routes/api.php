7ab2fa58d830db502c0c597631574147e37805fc
dolani <akindolaniayoola@gmail.com>
2017-11-16 10:16:49 +0100

    Update routes file

--- routes/api.php
+++ routes/api.php
@@ -77,6 +77,8 @@ $api->version('v1', function ($api) use($controllers){
         $api->post('/authenticate', [ 'prefix' => 'auth', 'uses' => $controllers['auth'].'@authenticate']);
         $api->get('/user/{user}', [ 'prefix' => 'frauds', 'uses' => $controllers['user'].'@userFraud']);
         $api->get('/fraud/{fraudId}', ['uses' => $controllers['fraud'].'@fraud']);
+
+        
         /**
         * User routes
         * Protected Routes
